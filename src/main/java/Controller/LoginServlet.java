bb726e63f574d3fd1fe29b3fdd9eb7a855a60c04
Utilisateur <Utilisateur@DESKTOP-IFQKUK1.home>
2018-12-15 17:29:29 +0100

    modif login servlet.

--- src/main/java/Controller/LoginServlet.java
+++ src/main/java/Controller/LoginServlet.java
@@ -8,16 +8,18 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet(name = "LoginServlet", urlPatterns = {"/servlet1"})
 public class LoginServlet extends HttpServlet {
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, SQLException {
-        
+
         String action = request.getParameter("action");
         System.out.println("ACTION " + action);
         if (action != null) {
@@ -41,28 +43,28 @@ public class LoginServlet extends HttpServlet {
         } else {
             showView("login_test.jsp", request, response);
         }
-        
+
     }
-    
+
     private void exitSession(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.invalidate();
         showView("login_test.jsp", request, response);
     }
-    
+
     private void startSession(HttpServletRequest request, HttpServletResponse response, String action) throws ServletException, IOException {
-        
+
         HttpSession session = request.getSession(false);
-        
+
         String log = request.getParameter("log");
         String mdp = request.getParameter("mdp");
-        
+
         if (log.equals("admin") && mdp.equals("admin")) {
-            
+
             showView("AdminProduct.jsp", request, response);
-            
+
         } else {
-            
+
             DAO dao = new DAO(DataSourceFactory.getDataSource());
             Customer c = null;
             try {
@@ -70,7 +72,7 @@ public class LoginServlet extends HttpServlet {
             } catch (SQLException ex) {
                 Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
             String email = c.getEmail();
             String id = Integer.toString(c.getCustomerId());
             session.setAttribute("id", c.getCustomerId());
@@ -81,7 +83,7 @@ public class LoginServlet extends HttpServlet {
             session.setAttribute("state", c.getState());
             session.setAttribute("city", c.getCity());
             session.setAttribute("credit", c.getCreditLimit());
-            
+
             if ((log == null ? email == null : log.equals(email)) && (mdp == null ? id == null : mdp.equals(id))) {
                 request.setAttribute("correct", true);
                 showView("client_side_view.jsp", request, response);
@@ -90,7 +92,7 @@ public class LoginServlet extends HttpServlet {
             }
         }
     }
-    
+
     private void showView(String jsp, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         getServletConfig().getServletContext().getRequestDispatcher("/" + jsp).forward(request, response);
     }
