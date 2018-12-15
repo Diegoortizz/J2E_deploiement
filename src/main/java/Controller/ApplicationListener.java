f4ef42211a19ab6946f288220c46f999a5276637
Utilisateur <Utilisateur@DESKTOP-IFQKUK1.home>
2018-12-15 19:41:07 +0100

    test2

--- src/main/java/Controller/ApplicationListener.java
+++ src/main/java/Controller/ApplicationListener.java
@@ -9,13 +9,11 @@ import Modele.DataSourceFactory;
 import Modele.DAO;
 import Modele.Discount;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -26,59 +24,56 @@ import org.apache.derby.tools.ij;
  * @author Matthias
  */
 @WebListener()
-public class ApplicationListener implements ServletContextListener{
+public class ApplicationListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        if (!databaseExists()){
+        if (!databaseExists()) {
             initializeDatabase();
         }
-        
-        ServletContext context = sce.getServletContext();
-        DAO dao = new DAO(DataSourceFactory.getDataSource());
-        context.setAttribute("dat", dao);
-        
+
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");  //To change body of generated methods, choose Tools | Templates.
     }
-       
+
     private boolean databaseExists() {
-		boolean result = false;
+        boolean result = false;
 
-		DAO dao = new DAO(DataSourceFactory.getDataSource());
-		try {
-			List<Discount> allCodes = dao.allCodes();
-			Logger.getLogger("ProjetJEE").log(Level.INFO, "Database already exists");
-			result = true;
-		} catch (SQLException ex) {
-			Logger.getLogger("ProjetJEE").log(Level.INFO, "Database does not exist");
-		} 
-		return result;
-	}
+        try {
+            DAO dao = new DAO(DataSourceFactory.getDataSource());
 
-	private void initializeDatabase() {
-		OutputStream nowhere = new OutputStream() {
-			@Override
-			public void write(int b) {
-			}
-		};
-		
-		Logger.getLogger("ProjetJEE").log(Level.INFO, "Creating databse from SQL script");
-		try {
-			Connection connection = DataSourceFactory.getDataSource().getConnection();
-			int result = ij.runScript(connection, this.getClass().getResourceAsStream("export.sql"), "UTF-8", System.out, "UTF-8");
-			if (result == 0) {
-				Logger.getLogger("ProjetJEE").log(Level.INFO, "Database succesfully created");
-			} else {
-				Logger.getLogger("ProjetJEE").log(Level.SEVERE, "Errors creating database");
-			}
+            List<Discount> allCodes = dao.allCodes();
+            Logger.getLogger("ProjetJEE").log(Level.INFO, "Database already exists");
+            result = true;
+        } catch (SQLException ex) {
+            Logger.getLogger("ProjetJEE").log(Level.INFO, "Database does not exist");
+        }
+        return result;
+    }
 
-		} catch (UnsupportedEncodingException | SQLException e) {
-			Logger.getLogger("ProjetJEE").log(Level.SEVERE, null, e);
-		}
+    private void initializeDatabase() {
+        OutputStream nowhere = new OutputStream() {
+            @Override
+            public void write(int b) {
+            }
+        };
 
-	}
+        Logger.getLogger("ProjetJEE").log(Level.INFO, "Creating databse from SQL script");
+        try {
+            Connection connection = DataSourceFactory.getDataSource().getConnection();
+            int result = ij.runScript(connection, this.getClass().getResourceAsStream("export.sql"), "UTF-8", System.out /* nowhere */, "UTF-8");
+            if (result == 0) {
+                Logger.getLogger("ProjetJEE").log(Level.INFO, "Database succesfully created");
+            } else {
+                Logger.getLogger("ProjetJEE").log(Level.SEVERE, "Errors creating database");
+            } 
+
+        } catch (Exception e) {
+            Logger.getLogger("ProjetJEE").log(Level.SEVERE, null, e);
+        }
+
+    }
 }
