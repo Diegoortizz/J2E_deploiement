87191a78d18a870d9b9e4340066548f50cdd5582
Utilisateur <Utilisateur@DESKTOP-IFQKUK1.home>
2018-12-17 17:56:58 +0100

    controleur sql

--- src/main/java/Controller/ApplicationListener.java
+++ src/main/java/Controller/ApplicationListener.java
@@ -1,24 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import Modele.DAO;
+import Modele.DAOException;
 import Modele.DataSourceFactory;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import org.apache.derby.tools.ij;
 
-@WebListener
-public class ApplicationListener implements ServletContextListener {
+/**
+ *
+ * @author pedago
+ */
+@WebListener()
+@WebServlet(name = "ApplicationListener", urlPatterns = {"/ApplicationListener"})
+public class ApplicationListener extends HttpServlet implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -29,10 +36,18 @@ public class ApplicationListener implements ServletContextListener {
         } catch (SQLException ex) {
             Logger.getLogger(ApplicationListener.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        ServletContext context = sce.getServletContext();
+
+        DAO dao = new DAO(DataSourceFactory.getDataSource());
+
+        context.setAttribute("dao", dao);
+
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     private boolean databaseExists() throws SQLException {
@@ -41,24 +56,24 @@ public class ApplicationListener implements ServletContextListener {
     }
 
     private void initializeDatabase() {
-        Logger.getLogger("ProjetJEE").log(Level.INFO, "Creating embedded database...");
+        OutputStream nowhere = new OutputStream() {
+            @Override
+            public void write(int b) {
+            }
+        };
+
+        Logger.getLogger("ProjetJEE").log(Level.INFO, "Creating databse from SQL script");
         try {
             Connection connection = DataSourceFactory.getDataSource().getConnection();
-            int result = ij.runScript(
-                    connection,
-                    this.getClass().getResourceAsStream("export.sql"),
-                    "UTF-8",
-                    System.out,
-                    "UTF-8"
-            );
+            int result = ij.runScript(connection, this.getClass().getResourceAsStream("export.sql"), "UTF-8", System.out, "UTF-8");
             if (result == 0) {
                 Logger.getLogger("ProjetJEE").log(Level.INFO, "Database succesfully created");
             } else {
                 Logger.getLogger("ProjetJEE").log(Level.SEVERE, "Errors creating database");
             }
         } catch (UnsupportedEncodingException | SQLException e) {
-            Logger.getLogger("ProjetJEE").log(Level.SEVERE, "Errors creating database");
             Logger.getLogger("ProjetJEE").log(Level.SEVERE, null, e);
         }
     }
+
 }
